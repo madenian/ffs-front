23a95155ffdc79b9f0aa34985ede8ffe422541ad
Adrien AGARD <agardadrien@gmail.com>
2023-09-12 16:42:08 +0200

    fetch OK, travail sur le .map

--- components/Home.js
+++ components/Home.js
@@ -6,36 +6,83 @@ import { useEffect, useState } from "react";
 import { Bearer, serverAdress, twitchClientId } from "../ffs-tools";
 
 function Home() {
-  const streamersId = [];
+  const [streamers, setStreamers] = useState([]);
 
-  fetch(`${serverAdress}/streamers`)
-    .then((response) => response.json())
-    .then((data) => {
-     
-      for (let i = 0; i < data.length; i++) {
-        console.log(data[i].twitchId)
-        fetch(
-          `https://api.twitch.tv/helix/schedule?broadcaster_id=${data[i].twitchId}`,
-          {
-            method: "GET",
-            headers: {
-              "Client-Id": "yezyu92gzd8s4bpqxyrl1r893te09j",
-              'Authorization' : `Bearer c4rczh7a6nniomgrnd42sybt14ivcw`,
-            },
-          }
-        )
-          .then((response) => response.json())
-          .then((schedule) => {
-            console.log('planning', schedule
+  useEffect(() => {
+    fetch(`${serverAdress}/streamers`)
+      .then((response) => response.json())
+      .then(async (data) => {
+        const updatedStreamers = [];
+        for (let i = 0; i < data.length; i++) {
+          try {
+            const response = await fetch(
+              `https://api.twitch.tv/helix/schedule?broadcaster_id=${data[i].twitchId}`,
+              {
+                method: "GET",
+                headers: {
+                  "Client-Id": "yezyu92gzd8s4bpqxyrl1r893te09j",
+                  Authorization: `Bearer c4rczh7a6nniomgrnd42sybt14ivcw`,
+                },
+              }
             );
-            console.log('streamers', data[i].name)
-          });
-      
-      } 
-       
-      
-    });
+            const schedule = await response.json();
+            if (!schedule.error) {
+              updatedStreamers.push({
+                data: data[i],
+                planning: schedule.data.segments,
+              });
+            }
+          } catch (error) {
+            console.error("Error fetching schedule:", error);
+          }
+        }
+        setStreamers(updatedStreamers);
+        console.log("updatedstreamers", updatedStreamers);
+      });
+    console.log("streamers", streamers);
+  }, []);
 
+  /*
+  streamers.map((streamer) => {
+    
+    name : streamers.data.name;
+    photo: streamers.data.profileImage;
+    
+  const planning= streamers.data.planning.map{
+    date: streamers.data.planning.date;
+
+
+  }
+  });
+
+
+  exemple planning : planning
+: 
+Array(20)
+0
+: 
+canceled_until
+: 
+null
+category
+: 
+null
+end_time
+: 
+"2023-09-13T22:00:00Z"
+id
+: 
+"eyJzZWdtZW50SUQiOiJiMmNjN2VhYi01NTczLTRhY2QtOWJlZi1mMDI0OTBmOTdmODEiLCJpc29ZZWFyIjoyMDIzLCJpc29XZWVrIjozN30="
+is_recurring
+: 
+true
+start_time
+: 
+"2023-09-13T18:00:00Z"
+title
+: 
+"La grosse soirée"
+*/
   return (
     <div>
       <Nav></Nav>
@@ -43,6 +90,7 @@ function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        {/* {streamers} */}
       </main>
     </div>
   );

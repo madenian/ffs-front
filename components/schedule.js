a9782d9d427a149ef3d7f679d21e856a46f1675e
Thibaud <abonnel.t@gmail.com>
2023-09-13 15:50:23 +0200

    v1 schedule

--- components/schedule.js
+++ components/schedule.js
@@ -1,16 +1,41 @@
-// import styles from "../styles/Schedule.module.css";
+
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import styles from "../styles/Schedule.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitch, faEye } from "@fortawesome/free-solid-svg-icons";
 
 function Schedule({ name, title, id, start, end, category, image }) {
 
+    const twitchUrl = `https://www.twitch.tv/${name}`;
 
   return (
-   <div>
-    {name}
-    {title}
-    {start}
-    {end}
+   <div className={styles.CardContainer}>
+    <div className={styles.ImageContainer}>
+        <img src={image} className = {styles.StreamImage}></img>
+    <a href={twitchUrl} target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon
+            icon={faEye}
+            beat
+            style={{ color: "#be5cff", cursor: "pointer" }}
+          />
+        </a>
+        {/* <FontAwesomeIcon icon={faTwitch} beat style={{color: "#be5cff",}} /> */}
+        
+    </div>
+    <div className={styles.DescriptionContainer}>
+        <div className={styles.DescriptionLine}>{name}</div>
+        <div className={styles.DescriptionLine}>{title}</div>
+        <div className={styles.DescriptionLine}>{category}</div>
+        <div>{start}</div>
+        <div>{end}  </div>
+       
+       
+        
+       
+          
+    </div>
+    
    </div>
 
   );
